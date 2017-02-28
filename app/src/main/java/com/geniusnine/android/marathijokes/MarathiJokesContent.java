83d96c0d4f48de5cf3d7f66a9962254e0b65d3c0
geniusninedev <developer@nineinfosys.com>
2017-02-28 14:56:32 +0530

    Added

---
+++ app/src/main/java/com/geniusnine/android/marathijokes/MarathiJokesContent.java
@@ -0,0 +1,42 @@
+package com.geniusnine.android.marathijokes;
+
+/**
+ * Created by AndriodDev8 on 20-02-2017.
+ */
+
+public class MarathiJokesContent {
+
+    @com.google.gson.annotations.SerializedName("id")
+    private String id ;
+    @com.google.gson.annotations.SerializedName("category")
+    private String category;
+    @com.google.gson.annotations.SerializedName("content")
+    private String content;
+
+    public MarathiJokesContent() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
