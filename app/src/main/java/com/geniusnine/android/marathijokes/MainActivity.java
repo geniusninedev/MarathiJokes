687b404400dce67b8593497678accf276bbb78b4
geniusninedev <developer@nineinfosys.com>
2017-03-03 04:41:19 +0530

    new config final with ad

--- app/src/main/java/com/geniusnine/android/marathijokes/MainActivity.java
+++ app/src/main/java/com/geniusnine/android/marathijokes/MainActivity.java
@@ -45,10 +45,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MobileAds.initialize(getApplicationContext(), "ca-app-pub-3940256099942544~3347511713");
-        AdView mAdView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
+
         setupCategoryListView();
     }
 
